c246fd91321806c1171f84ef6e143e4729ec9dab
thnsmmrs <53453187+thnsmmrs@users.noreply.github.com>
2025-11-02 18:16:58 -0500

    Refactor plotting and spline interpolation in PA2.py
    First version with working graphing pre clicking

--- PA2.py
+++ PA2.py
@@ -1,28 +1,43 @@
 import numpy
-import matplotlib
+import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 
 L1 = 1.0
 L2 = 0.5
+min,max = 0.5,1.5
 
-px = numpy.array(5)
-py = numpy.array(5)
+px = numpy.array([0.6,1.1,1.2,0.8,0.1])
+py = numpy.array([0.9,0.2,-0.5,-0.9,-0.6])
 #DEBUG: print(px, " ", py)
 
 #Spline
-cs = CubicSpline(x,y)
-xs = numpy.linspace(x[0]-0.5, x[-1]+0.5, 400)
 
-fig, ax = plt.subplots(figsize=(6.5, 4))
-ax.plot(x, y, 'o', label='data')
-ax.plot(xs, cs(xs), label='S(x)')
-ax.plot(xs, cs(xs, 1), label="S'(x)")
-ax.plot(xs, cs(xs, 2), label="S''(x)")
-ax.plot(xs, cs(xs, 3), label="S'''(x)")
-ax.set_xlim(x[0]-0.5, x[-1]+0.5)
-ax.legend(loc='best')
-ax.grid(True)
+#arc params (from SciPy documentation)
+ds = numpy.hypot(numpy.diff(px), numpy.diff(py))
+s = numpy.concatenate(([0.0], numpy.cumsum(ds)))
+
+#Spline sampling (for smooth connection)
+sx = CubicSpline(s, px)
+sy = CubicSpline(s, py)
+su = numpy.linspace(s[0],s[-1],400)
+xs = sx(su)
+ys = sy(su)
+
+#defining plotting standards
+t = numpy.linspace(0, 2*numpy.pi, 1000)
+x_min, y_min = min*numpy.cos(t), min*numpy.sin(t)
+x_max, y_max = max*numpy.cos(t), max*numpy.sin(t)
+
+fig, ax = plt.subplots()
+ax.plot(x_min, y_min)
+ax.plot(x_max, y_max)
+ax.plot(px, py, 'x')
+ax.plot(xs, ys)
+
+ax.set_xlim(-1*max,max)
+ax.set_ylim(-1*max,max)
 plt.show()
+
 
 r = numpy.sqrt(px**2 + py**2)
 theta1 = numpy.arctan2(py,px) - numpy.arccos((r**2+L1**2-L2**2)/(2*r*L1))
