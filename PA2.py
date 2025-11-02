b5f1563d4f504f6b9bdb77e25c8fa13af2885f1e
thnsmmrs <53453187+thnsmmrs@users.noreply.github.com>
2025-11-02 17:51:29 -0500

    Refactor input and implement spline interpolation
    Updated input handling and added spline interpolation for data visualization.

--- PA2.py
+++ PA2.py
@@ -1,16 +1,31 @@
 import numpy
+import matplotlib
 from scipy.interpolate import CubicSpline
-
 
 L1 = 1.0
 L2 = 0.5
-px = float(input("Enter px: "))
-py = float(input("Enter py: "))
-print(px, " ", py)
 
+px = numpy.array(5)
+py = numpy.array(5)
+#DEBUG: print(px, " ", py)
+
+#Spline
+cs = CubicSpline(x,y)
+xs = numpy.linspace(x[0]-0.5, x[-1]+0.5, 400)
+
+fig, ax = plt.subplots(figsize=(6.5, 4))
+ax.plot(x, y, 'o', label='data')
+ax.plot(xs, cs(xs), label='S(x)')
+ax.plot(xs, cs(xs, 1), label="S'(x)")
+ax.plot(xs, cs(xs, 2), label="S''(x)")
+ax.plot(xs, cs(xs, 3), label="S'''(x)")
+ax.set_xlim(x[0]-0.5, x[-1]+0.5)
+ax.legend(loc='best')
+ax.grid(True)
+plt.show()
 
 r = numpy.sqrt(px**2 + py**2)
 theta1 = numpy.arctan2(py,px) - numpy.arccos((r**2+L1**2-L2**2)/(2*r*L1))
 theta2 = numpy.pi - numpy.arccos((L1**2+L2**2-r**2)/(2*L1*L2))
 
-print (theta1, " ", theta2)
+#DEBUG: print (theta1, " ", theta2)
