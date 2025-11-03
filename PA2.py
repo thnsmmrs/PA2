fcfe0d3389bc3aedcb92e94cce8ac4797907b0c9
thnsmmrs <53453187+thnsmmrs@users.noreply.github.com>
2025-11-02 19:48:09 -0500

    Refactor plotting logic and input handling
    Refactored plotting code to streamline point input and visualization.

--- PA2.py
+++ PA2.py
@@ -6,8 +6,23 @@ L1 = 1.0
 L2 = 0.5
 min,max = 0.5,1.5
 
-px = numpy.array([0.6,1.1,1.2,0.8,0.1])
-py = numpy.array([0.9,0.2,-0.5,-0.9,-0.6])
+#defining plotting standards
+t = numpy.linspace(0, 2*numpy.pi, 1000)
+x_min, y_min = min*numpy.cos(t), min*numpy.sin(t)
+x_max, y_max = max*numpy.cos(t), max*numpy.sin(t)
+
+fig, graph = plt.subplots()
+graph.plot(x_min, y_min)
+graph.plot(x_max, y_max)
+graph.set_xlim(-1*max,max)
+graph.set_ylim(-1*max,max)
+plt.title("Click 5 points inside the workspace")
+points = plt.ginput(5, timeout=-1)
+
+points = numpy.array(points)
+
+px = points[:, 0]
+py = points[:, 1]
 #DEBUG: print(px, " ", py)
 
 #Spline
@@ -23,21 +38,9 @@ su = numpy.linspace(s[0],s[-1],400)
 xs = sx(su)
 ys = sy(su)
 
-#defining plotting standards
-t = numpy.linspace(0, 2*numpy.pi, 1000)
-x_min, y_min = min*numpy.cos(t), min*numpy.sin(t)
-x_max, y_max = max*numpy.cos(t), max*numpy.sin(t)
-
-fig, ax = plt.subplots()
-ax.plot(x_min, y_min)
-ax.plot(x_max, y_max)
-ax.plot(px, py, 'x')
-ax.plot(xs, ys)
-
-ax.set_xlim(-1*max,max)
-ax.set_ylim(-1*max,max)
+graph.plot(px, py, 'x')
+graph.plot(xs, ys)
 plt.show()
-
 
 r = numpy.sqrt(px**2 + py**2)
 theta1 = numpy.arctan2(py,px) - numpy.arccos((r**2+L1**2-L2**2)/(2*r*L1))
