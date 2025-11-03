3c0158de9f18defaa93696a90a2e4caf0c9adcb6
krishm90210 <krishm90210@gmail.com>
2025-11-03 18:58:36 -0500

    Update PA2.py
    just converted all numpy to np to make it easier

--- PA2.py
+++ PA2.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 
@@ -7,9 +7,9 @@ L2 = 0.5
 min,max = abs(L1-L2), L1+L2
 
 #defining plotting standards
-t = numpy.linspace(0, 2*numpy.pi, 1000)
-x_min, y_min = min*numpy.cos(t), min*numpy.sin(t)
-x_max, y_max = max*numpy.cos(t), max*numpy.sin(t)
+t = np.linspace(0, 2*np.pi, 1000)
+x_min, y_min = min*np.cos(t), min*np.sin(t)
+x_max, y_max = max*np.cos(t), max*np.sin(t)
 
 fig, graph = plt.subplots()
 graph.plot(x_min, y_min)
@@ -19,7 +19,7 @@ graph.set_ylim(-1*max,max)
 plt.title("Click 5 points inside the workspace")
 points = plt.ginput(5, timeout=-1)
 
-points = numpy.array(points)
+points = np.array(points)
 
 px = points[:, 0]
 py = points[:, 1]
@@ -28,13 +28,13 @@ py = points[:, 1]
 #Spline
 
 #arc params (from SciPy documentation)
-ds = numpy.hypot(numpy.diff(px), numpy.diff(py))
-s = numpy.concatenate(([0.0], numpy.cumsum(ds)))
+ds = np.hypot(np.diff(px), np.diff(py))
+s = np.concatenate(([0.0], np.cumsum(ds)))
 
 #Spline sampling (for smooth connection)
 sx = CubicSpline(s, px)
 sy = CubicSpline(s, py)
-su = numpy.linspace(s[0],s[-1],400)
+su = np.linspace(s[0],s[-1],400)
 xs = sx(su)
 ys = sy(su)
 
@@ -42,8 +42,8 @@ graph.plot(px, py, 'x')
 graph.plot(xs, ys)
 plt.show()
 
-r = numpy.sqrt(px**2 + py**2)
-theta1 = numpy.arctan2(py,px) - numpy.arccos((r**2+L1**2-L2**2)/(2*r*L1))
-theta2 = numpy.pi - numpy.arccos((L1**2+L2**2-r**2)/(2*L1*L2))
+r = np.sqrt(px**2 + py**2)
+theta1 = np.arctan2(py,px) - np.arccos((r**2+L1**2-L2**2)/(2*r*L1))
+theta2 = np.pi - np.arccos((L1**2+L2**2-r**2)/(2*L1*L2))
 
 #DEBUG: print (theta1, " ", theta2)
