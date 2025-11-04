0c70a7aecaca3d7a1c4171ad98d91adbbfcd37f4
thnsmmrs <53453187+thnsmmrs@users.noreply.github.com>
2025-11-04 16:14:48 -0500

    Refactor PA2.py for clarity and maintainability
    Refactor code for clarity and maintainability, including renaming variables and adding comments for better understanding.

--- PA2.py
+++ PA2.py
@@ -6,11 +6,12 @@ L1 = 1.0
 L2 = 0.5
 min,max = abs(L1-L2), L1+L2
 
-#defining plotting standards
+#defining max and min work space (circles)
 t = np.linspace(0, 2*np.pi, 1000)
 x_min, y_min = min*np.cos(t), min*np.sin(t)
 x_max, y_max = max*np.cos(t), max*np.sin(t)
 
+#defining graph based on max and min provided in problem
 fig, graph = plt.subplots()
 graph.plot(x_min, y_min)
 graph.plot(x_max, y_max)
@@ -18,33 +19,32 @@ graph.set_xlim(-1*max,max)
 graph.set_ylim(-1*max,max)
 plt.title("Click 5 points inside the workspace")
 points = plt.ginput(5, timeout=-1)
-
 points = np.array(points)
-
+#Ayush include source
 px = points[:, 0]
 py = points[:, 1]
 #DEBUG: print(px, " ", py)
 
-#Spline
+#Spline creation
 
-#arc params (from SciPy documentation)
+#arc params (from SciPy documentation attached on git)
 ds = np.hypot(np.diff(px), np.diff(py))
 s = np.concatenate(([0.0], np.cumsum(ds)))
 
 #Spline sampling (for smooth connection)
-sx = CubicSpline(s, px)
-sy = CubicSpline(s, py)
+splinepoint_px = CubicSpline(s, px)
+splinepoint_py = CubicSpline(s, py)
 su = np.linspace(s[0],s[-1],400)
-xs = sx(su)
-ys = sy(su)
+x_splinetrace = splinepoint_px(su)
+y_splinetrace = splinepoint_py(su)
 
 graph.plot(px, py, 'x')
-graph.plot(xs, ys)
-plt.pause(0.5)
+graph.plot(x_splinetrace, y_splinetrace)
+plt.pause(0.5) #makes it so the figure doesn't have to close for sim to work
 
-r = np.sqrt(px**2 + py**2)
-theta1 = np.arctan2(py,px) - np.arccos((r**2+L1**2-L2**2)/(2*r*L1))
-theta2 = np.pi - np.arccos((L1**2+L2**2-r**2)/(2*L1*L2))
+r = np.sqrt(x_splinetrace**2 + y_splinetrace**2)
+theta1_array = np.arctan2(y_splinetrace,x_splinetrace) - np.arccos((r**2+L1**2-L2**2)/(2*r*L1))
+theta2_array = np.pi - np.arccos((L1**2+L2**2-r**2)/(2*L1*L2))
 
 #DEBUG: print (theta1, " ", theta2)
 
@@ -59,27 +59,51 @@ link1, = graph.plot([], [])
 link2, = graph.plot([], [])
 trace, = graph.plot([], [])
 
-outOfBoundsMax = False
 outOfBoundsMin = False
+outOfBoundsMax = False
+
 
 
 for i in range(len(fk_x1)):
     #Matplotlib animation documentation used for set data functions (originally was plotting all points)
-    link1.set_data([0, fk_x1[i]], [0, fk_y1[i]])
-    link2.set_data([fk_x1[i], fk_x2[i]], [fk_y1[i], fk_y2[i]])
-    trace.set_data([fk_x2[i]], [fk_y2[i]])
-    plt.pause(0.001) #Change time var based on how fast/slow we want the sim
+    if(outOfBoundsMax == 0 and outOfBoundsMin == 0):
+        link1.set_data([0, fk_x1[i]], [0, fk_y1[i]])
+        link2.set_data([fk_x1[i], fk_x2[i]], [fk_y1[i], fk_y2[i]])
+        trace.set_data([fk_x2[i]], [fk_y2[i]])
+        plt.pause(0.001) #Change time var based on how fast/slow we want the sim
 
     #Boundary conditions (DEBUG)
     link2_mag = np.sqrt(fk_x2[i]**2 + fk_y2[i]**2)
     link2_mag_prev = np.sqrt(fk_x2[i-1]**2 + fk_y2[i-1]**2)
+    #DEBUG: print(x_splinetrace[i], y_splinetrace[i], "\n")
 
-
-    print(link2_mag,"\n")
+    #DEBUG: print(link2_mag,"\n")
     if (np.isnan(link2_mag) and link2_mag_prev > L1 or outOfBoundsMax):
         outOfBoundsMax = True
-        print("out of bounds (max)")
-    elif(link2_mag > min and link2_mag < max):
-        outOfBoundsMin = False  
-        outOfBoundsMax = False
+        #DEBUG: print("out of bounds (max)")
+        thetaOutMax = np.arctan2(y_splinetrace[i], x_splinetrace[i])
+        #print(thetaOutMax,"\n")
+        #plots links with angle of spline that should be traced
+        link1.set_data([0,L1*np.cos(thetaOutMax)], [0,L1*np.sin(thetaOutMax)])
+        link2.set_data([L1*np.cos(thetaOutMax),L1*np.cos(thetaOutMax)+L2*(np.cos(thetaOutMax))],[L1*np.sin(thetaOutMax),L1*np.sin(thetaOutMax)+L2*(np.sin(thetaOutMax))])
+        trace.set_data([L1*np.cos(thetaOutMax)+L2*(np.cos(thetaOutMax))],[L1*np.sin(thetaOutMax)+L2*(np.sin(thetaOutMax))])
+        plt.pause(0.001)
+        if (np.isnan(link2_mag) == 0):
+            outOfBoundsMax = False
+
+    print(outOfBoundsMin)
+    if (np.isnan(link2_mag) and link2_mag_prev < L1 or outOfBoundsMin):
+        outOfBoundsMin = True
+        print("out of bounds (min)")
+        thetaOutMin = np.arctan2(y_splinetrace[i], x_splinetrace[i])
+        print(thetaOutMin,"\n")
+        #plots links with angle of spline that should be traced
+        link1.set_data([0,L1*np.cos(thetaOutMin)], [0,L1*np.sin(thetaOutMin)])
+        link2.set_data([L1*np.cos(thetaOutMin),L1*np.cos(thetaOutMin)-L2*(np.cos(thetaOutMin))],[L1*np.sin(thetaOutMin),L1*np.sin(thetaOutMin)-L2*(np.sin(thetaOutMin))])
+        trace.set_data([L1*np.cos(thetaOutMin)-L2*(np.cos(thetaOutMin))],[L1*np.sin(thetaOutMin)-L2*(np.sin(thetaOutMin))])
+        plt.pause(0.001)
+        if (np.isnan(link2_mag) == 0):
+           outOfBoundsMin = False
+
+
 plt.show()
